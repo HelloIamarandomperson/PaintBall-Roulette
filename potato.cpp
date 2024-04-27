229c8ace30f4b121fe9df393a3872630a6d44fea
HelloIamarandomperson <136181387+HelloIamarandomperson@users.noreply.github.com>
2024-04-27 18:39:57 -0400

    Update potato.cpp

--- potato.cpp
+++ potato.cpp
@@ -103,24 +103,7 @@ int main() {
         }
     }
 }
-void checkMag(char cylinder[]) {
-    for (int i = 0; i < slots; i++) {
-        if (cylinder[i] == '\0') {
-            printf("0 ");
-        } else {
-            printf("%c ", cylinder[i]);
-        }
-    }
-    printf("\n");
-}
 
-
-char OpponentOrYou() {
-    char response;
-    scanf("%c", &response);
-    response = tolower(response);
-    return response;
-}
 
 
 
