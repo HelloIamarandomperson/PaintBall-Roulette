13584df63304013504728df6f199e901746210e4
HelloIamarandomperson <136181387+HelloIamarandomperson@users.noreply.github.com>
2024-05-07 14:49:12 -0400

    made game properly end

--- potato.cpp
+++ potato.cpp
@@ -92,9 +92,11 @@ int main() {
                 } else {
                     if (yourHealth < 0) {
                         printf("You are dead. Should'nt have shot yourself probably. L+ratio+skill-issue");
+                        return 0;
                         break;
                     } else {
                         printf("You killed the poor innocent other person. Hooray for you.");
+                        return 0;
                         break;
                     }
                 }
