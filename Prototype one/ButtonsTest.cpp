b093deb7b86031b1da84be3c3123af8b44b2834b
HelloIamarandomperson <136181387+HelloIamarandomperson@users.noreply.github.com>
2024-05-17 14:28:07 -0400

    Increased button efficiency

--- Prototype one/ButtonsTest.cpp
+++ Prototype one/ButtonsTest.cpp
@@ -14,20 +14,16 @@ int Button() {
   event_queue = al_create_event_queue();
   al_register_event_source(event_queue, al_get_mouse_event_source());
 
-  al_clear_to_color(al_map_rgb(0, 0, 0));\
-  al_draw_rectangle(500, 500, 800, 800, al_map_rgb(200, 200, 0), 10);
+  al_draw_rectangle(0, 600, 200, 800, al_map_rgb(200, 200, 0), 10);
   // first 2 is coords for the top vertices's's, next 2 is for the bottom one.
   al_flip_display();
 
   while (!select) {
     ALLEGRO_EVENT ButtonEvent;
     al_wait_for_event(event_queue, & ButtonEvent);
-    if (ButtonEvent.mouse.x >= 500 && ButtonEvent.mouse.y >= 500 && ButtonEvent.mouse.x <= 800 && ButtonEvent.mouse.y <= 800 && ButtonEvent.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-      printf("Hooray!(%d, %d)", ButtonEvent.mouse.x, ButtonEvent.mouse.y);
+    if (ButtonEvent.mouse.x >= 0 && ButtonEvent.mouse.y >= 600 && ButtonEvent.mouse.x <= 200 && ButtonEvent.mouse.y <= 800 && ButtonEvent.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
       select = true;
-    } else if (ButtonEvent.mouse.x >= 500 && ButtonEvent.mouse.y >= 500 && ButtonEvent.mouse.x <= 800 && ButtonEvent.mouse.y <= 800)
-      printf("(%d, %d)", ButtonEvent.mouse.x, ButtonEvent.mouse.y);
+    }
   }
-
   return 0;
 }
